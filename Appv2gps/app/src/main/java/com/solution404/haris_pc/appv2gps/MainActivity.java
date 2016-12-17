a6ca12610c26e3586dc18e213ebdf9badb528be9
Haris-PC <harismuha123@gmail.com>
2016-12-17 23:37:27 +0100

    new files, old app
    Android app working for TEST

---
+++ Appv2gps/app/src/main/java/com/solution404/haris_pc/appv2gps/MainActivity.java
@@ -0,0 +1,35 @@
+package com.solution404.haris_pc.appv2gps;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.solution404.haris_pc.appservicegps.R;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, GPS_Service.class);
+        startService(intent);
+
+
+
+
+    }
+
+}
